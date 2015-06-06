8d943fb3b4419f88ee5ef605abee63f6e8070e5f
Michał <dylesiu@gmail.com>
2015-06-06 02:01:59 +0200

    Poprawienie jednej tabeli + Backup

--- InitScripts/Transations.sql
+++ InitScripts/Transations.sql
@@ -3,5 +3,8 @@ use CinemaDatabase
 create table Transations(
 transationID int primary key IDENTITY(1,1),
 showID int null,
+employeeID int not null, 
 transationDate date not null DEFAULT getDate()
-FOREIGN KEY (showID) REFERENCES Shows(showID));
+FOREIGN KEY (showID) REFERENCES Shows(showID)
+FOREIGN KEY (employeeID) REFERENCES Employees(employeeID)
+);
